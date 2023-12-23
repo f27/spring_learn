05003c9110a13f835f6fb1d42a06db68b902c8d9
Farit Aznabaev <fazanbaev@qa.guru>
2023-12-24 00:23:47 +0500

    dockerfile

--- bobr-front/run.sh
+++ bobr-front/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker build --build-arg NPM_COMMAND="build:dev" -t front:latest .
+docker build --build-arg NPM_COMMAND="build:dev" -t front:1.0.0 -t front:latest .
 docker run --name front -dp 3000:80 front:latest
